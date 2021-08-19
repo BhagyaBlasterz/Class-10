7f56a62fd6c1f1eb8dcb32a82096dd58615a65ed
bhagya313 <61934390+bhagya313@users.noreply.github.com>
2021-08-19 20:09:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+//var a = prompt('Enter the first variable:');
+var trex,ground
+
+function preload() {
+  
+}
+
+function setup(){
+
+  createCanvas(400,400)
+  trex = createSprite(200,200)
+
+  }
+
+
+function draw(){
+  background=("#440000")
+  if (keyDown("space")) {
+    trex.x=trex.x+10
+    
+  }
+drawSprites;
+
+
+}
